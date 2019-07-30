525119c262223194a50c2c55621936eb270fe686
zenos <zenoslin@outlook.com>
2019-07-30 11:02:43 +0800

    ADDED: 接口节流

---
+++ JavaScript/防抖和节流/apiThrottle.js
@@ -0,0 +1,29 @@
+/**
+ * @author       Zenos Lin <zenoslin@outlook.com>
+ * @copyright    2019 Zenos Lin.
+ */
+
+const canRun = {};
+
+/**
+ * @function throttle 节流执行接口函数（Promise）
+ *
+ * @param {function} fn 需节流执行的Promise函数
+ * @param {array} list 参数列表
+ * @param {number} throttleCode 节流成功code
+ *
+ * @return {Promise} 返回函数内部Promise
+ */
+export default function throttle(fn, list = [], throttleCode = 9999) {
+  if (canRun[fn.name]) return Promise.reject(throttleCode);
+  canRun[fn.name] = true;
+  return fn(...list)
+    .then(res => {
+      canRun[fn.name] = false;
+      return res;
+    })
+    .catch(err => {
+      canRun[fn.name] = false;
+      return err;
+    });
+}
