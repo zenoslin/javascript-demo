5ef644998c1a64693ddbf71484d187b7e1cf2959
zenos <zenoslin@outlook.com>
2019-08-02 16:53:56 +0800

    UPDATE: 获取URL参数

--- JavaScript/URL参数/GetQuery.js
+++ JavaScript/URL参数/GetQuery.js
@@ -1,9 +1,20 @@
-function getQuery(queryName) {
-  const reg = new RegExp(`(^|&)${queryName}=([^&]*)(&|$)`, 'i');
-  // eslint-disable-next-line no-restricted-globals
-  const r = location.search.substr(1).match(reg);
-  if (r != null) return decodeURI(r[2]);
-  return null;
-}
+/**
+ * @author       Zenos Lin <zenoslin@outlook.com>
+ * @copyright    2019 Zenos Lin.
+ */
 
-export default getQuery;
+/**
+ * @function getQuery 获取URL参数
+ * 传入参数名获取当前地址栏上参数的值
+ * @param {String} name
+ * @return {String}
+ */
+export default function getQuery(name, url) {
+  if (!url) url = window.location.href;
+  name = name.replace(/[\[\]]/g, "\\$&");
+  var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)");
+  var results = regex.exec(url);
+  if (!results) return null;
+  if (!results[2]) return "";
+  return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
