7305be96af9cbbec8822d7d5f54d61356e23f591
zenos <zenoslin@outlook.com>
2020-03-19 21:52:19 +0800

    feat: :zap: 增加海外电话

--- typescript/regexp-box/src/main.ts
+++ typescript/regexp-box/src/main.ts
@@ -2,6 +2,11 @@ const intranetReg = (ip: string) => {
   return /^(172\.(1[6-9]|2\d|3[01])\.(1\d{2}|2[0-4]\d|25[0-5]|[1-9]\d|\d)\.(1\d{2}|2[0-4]\d|25[0-5]|[1-9]\d|\d))$|^(192\.168\.(1\d{2}|2[0-4]\d|25[0-5]|[1-9]\d|\d)\.(1\d{2}|2[0-4]\d|25[0-5]|[1-9]\d|\d))$/.test(ip);
 };
 
+const internationaPhoneReg = (number:string) => {
+    return /^[0-9]{7}$/.test(number);
+}
+
 export default {
-  intranetReg
+  intranetReg,
+  internationaPhoneReg
 };
