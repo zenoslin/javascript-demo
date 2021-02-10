31bdf088b51c752c9038962b79f6cc745b6ddfd3
zenos <zenoslin@outlook.com>
2021-02-10 15:56:26 +0800

    feat: :zap: 简单模版渲染

---
+++ JavaScript/模版引擎实现/templateRender.js
@@ -0,0 +1,30 @@
+/**
+ * @author       Zenos Lin <zenoslin@outlook.com>
+ * @copyright    2021 Zenos Lin.
+ */
+
+/**
+ * @name render 简单实现模版渲染
+ * @param {string} template 模版
+ * @param {object} data 数据
+ */
+const render = (template, data) => {
+  const reg = /\{\{(\w+)\}\}/; // 模板字符串正则
+  if (reg.test(template)) {
+    // 判断模板里是否有模板字符串
+    const name = reg.exec(template)[1]; // 查找当前模板里第一个模板字符串的字段
+    template = template.replace(reg, data[name]); // 将第一个模板字符串渲染
+    return render(template, data); // 递归的渲染并返回渲染后的结构
+  }
+  return template; // 如果模板没有模板字符串直接返回
+};
+
+const test = () => {
+  let template = "我是{{name}}，年龄{{age}}，性别{{sex}}";
+  let data = {
+    name: "姓名",
+    age: 18,
+  };
+  console.log(render(template, data)); // 我是姓名，年龄18，性别undefined
+};
+test();
