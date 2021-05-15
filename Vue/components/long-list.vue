6f27888b9be5ec6afe785928b4304fe86722fe01
akazenoslin <akazenoslin@tencent.com>
2021-05-15 09:54:42 +0800

    feat: :cake: add vue component `long-list`

---
+++ Vue/components/long-list.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="long-list" @scroll="onScroll"><slot></slot></div>
+</template>
+
+<script>
+export default {
+    name: 'longList',
+    props: {
+        loading: {
+            default: false,
+        },
+        finished: {
+            default: false,
+        },
+        loadDistance: {
+            default: 20,
+        },
+    },
+    methods: {
+        onScroll(event) {
+            if (this.loading || this.finished) return;
+            if (event.srcElement.scrollTop + event.srcElement.offsetHeight >= event.srcElement.scrollHeight - this.loadDistance) {
+                this.$emit('load');
+            }
+        },
+    },
+};
+</script>
+
+<style lang="less" scoped>
+.long-list {
+    overflow-x: hidden;
+    overflow-y: auto;
+}
+</style>
